23e694449a1c09560c93056920c96c4af6abf93b
rusefi <rusefillc@gmail.com>
2024-01-01 17:15:18 -0500

    reduce memory usage monitoring

--- board.mk
+++ board.mk
@@ -5,6 +5,9 @@ BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 DDEFS += -DFIRMWARE_ID=\"Paralela\"
 SHORT_BOARD_NAME=paralela
 
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
+
 # assign critical LED to a non-existent pin
 DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
 
