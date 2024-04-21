7042bb97ff24f51d65dacc96947b46da0ce00b7b
AlphaX <normsp@gmail.com>
2024-04-21 04:15:07 -0400

    Update board_configuration.cpp
    maybe this can make default tune useful?

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -11,3 +11,30 @@ Gpio getRunningLedPin() {
 Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
+
+static void setInjectorPins() {
+	engineConfiguration->injectionPins[0] = Gpio::E0;
+	engineConfiguration->injectionPins[1] = Gpio::E6;
+	engineConfiguration->injectionPins[2] = Gpio::D9;
+	engineConfiguration->injectionPins[3] = Gpio::E3;
+}
+
+static void setIgnitionPins() {
+	engineConfiguration->ignitionPins[0] = Gpio::A8;
+	engineConfiguration->ignitionPins[1] = Gpio::C9;
+	engineConfiguration->ignitionPins[2] = Gpio::A10;
+	engineConfiguration->ignitionPins[3] = Gpio::C8;
+}
+
+void setBoardDefaultConfiguration() {
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_0;
+	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+	engineConfiguration->iat.adcChannel = EFI_ADC_13;
+	engineConfiguration->afr.hwChannel = EFI_ADC_14;
+
+	engineConfiguration->vbattAdcChannel = EFI_ADC_12;
+	engineConfiguration->analogInputDividerCoefficient = 10.2f;
+	
+	engineConfiguration->triggerInputPins[0] = Gpio::B1;
+	engineConfiguration->triggerInputPins[1] = Gpio::A6;
+}
