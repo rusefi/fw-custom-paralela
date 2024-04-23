701d06703361c9baca4dea415f9e3c056af12775
AlphaX <normsp@gmail.com>
2024-04-23 03:19:20 -0400

    Update board_configuration.cpp
    ok didnt work

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -42,19 +42,19 @@ void setCustomVbatt() {
 	engineConfiguration->vbattAdcChannel = EFI_ADC_12;
 
 }
-
+/*
 static void setInjectorPins() {
-	engineConfiguration->injectionPins[0] = E0;
-	engineConfiguration->injectionPins[1] = E6;
-	engineConfiguration->injectionPins[2] = D9;
-	engineConfiguration->injectionPins[3] = E3;
+	engineConfiguration->injectionPins[0] = Gpio::E0;
+	engineConfiguration->injectionPins[1] = Gpio::E6;
+	engineConfiguration->injectionPins[2] = Gpio::D9;
+	engineConfiguration->injectionPins[3] = Gpio::E3;
 }
 
 static void setIgnitionPins() {
-	engineConfiguration->ignitionPins[0] = A8;
-	engineConfiguration->ignitionPins[1] = C9;
-	engineConfiguration->ignitionPins[2] = A10;
-	engineConfiguration->ignitionPins[3] = C8;
+	engineConfiguration->ignitionPins[0] = Gpio::A8;
+	engineConfiguration->ignitionPins[1] = Gpio::C9;
+	engineConfiguration->ignitionPins[2] = Gpio::A10;
+	engineConfiguration->ignitionPins[3] = Gpio::C8;
 }
 
 void setBoardDefaultConfiguration() {
@@ -63,7 +63,10 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->iat.adcChannel = EFI_ADC_13;
 	engineConfiguration->afr.hwChannel = EFI_ADC_14;
 
+	engineConfiguration->vbattAdcChannel = EFI_ADC_12;
+	engineConfiguration->analogInputDividerCoefficient = 10.2f;
 	
-	engineConfiguration->triggerInputPins[0] = B1;
-	engineConfiguration->triggerInputPins[1] = A6;
+	engineConfiguration->triggerInputPins[0] = Gpio::B1;
+	engineConfiguration->triggerInputPins[1] = Gpio::A6;
 }
+*/
