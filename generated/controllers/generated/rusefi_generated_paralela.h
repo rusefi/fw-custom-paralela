7a0146da03a255cdb3f81544e7e959b0566c35fb
GitHub git update Action <action@github.com>
2024-04-23 07:06:53 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_paralela.h
+++ generated/controllers/generated/rusefi_generated_paralela.h
@@ -1214,7 +1214,7 @@
 #define show_microRusEFI_presets false
 #define show_Proteus_presets false
 #define show_test_presets false
-#define SIGNATURE_HASH 2950460533
+#define SIGNATURE_HASH 732294902
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1511,7 +1511,7 @@
 #define ts_show_bank2_cam1 true
 #define ts_show_bank2_cam2 true
 #define ts_show_cam2 true
-#define ts_show_can2 true
+#define ts_show_can2 false
 #define ts_show_can_pins true
 #define ts_show_check_engine true
 #define ts_show_clt_iat_pullup true
@@ -1544,7 +1544,7 @@
 #define ts_show_fuel_strategy true
 #define ts_show_full_pinout true
 #define ts_show_gdi false
-#define ts_show_hardware_simulator true
+#define ts_show_hardware_simulator false
 #define ts_show_hd true
 #define ts_show_hip9011 false
 #define ts_show_idle_hardware true
@@ -1572,7 +1572,7 @@
 #define ts_show_sd_pins true
 #define ts_show_software_knock true
 #define ts_show_speedo_settings true
-#define ts_show_spi true
+#define ts_show_spi false
 #define ts_show_spi2_enable true
 #define ts_show_spi2_pins true
 #define ts_show_spi3_pins true
@@ -1591,7 +1591,7 @@
 #define ts_show_vr_threshold_all false
 #define ts_show_vr_threshold_pins true
 #define ts_show_vvt_output true
-#define TS_SIGNATURE "rusEFI default.2024.04.23.paralela.2950460533"
+#define TS_SIGNATURE "rusEFI default.2024.04.23.paralela.732294902"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_paralela.h
+++ generated/controllers/generated/signature_paralela.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2950460533
-#define TS_SIGNATURE "rusEFI default.2024.04.23.paralela.2950460533"
+#define SIGNATURE_HASH 732294902
+#define TS_SIGNATURE "rusEFI default.2024.04.23.paralela.732294902"
