691008d146156b4a326a072e4a89ff8119a2e58d
GitHub git update Action <action@github.com>
2024-09-06 21:13:56 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_paralela.h
+++ generated/controllers/generated/rusefi_generated_paralela.h
@@ -1302,7 +1302,7 @@
 #define show_Proteus_presets false
 #define show_test_presets false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2811028597
+#define SIGNATURE_HASH 88893421
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1622,7 +1622,7 @@
 #define ts_show_clutch_down_pin true
 #define ts_show_clutch_up true
 #define ts_show_clutch_up_pin true
-#define ts_show_communityCommsLedPid false
+#define ts_show_communityCommsLedPin false
 #define ts_show_compression_ratio false
 #define ts_show_compressor_sensor true
 #define ts_show_console_settings true
@@ -1750,7 +1750,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2024.09.05.paralela.2811028597"
+#define TS_SIGNATURE "rusEFI master.2024.09.06.paralela.88893421"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_paralela.h
+++ generated/controllers/generated/signature_paralela.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2811028597
-#define TS_SIGNATURE "rusEFI master.2024.09.05.paralela.2811028597"
+#define SIGNATURE_HASH 88893421
+#define TS_SIGNATURE "rusEFI master.2024.09.06.paralela.88893421"
