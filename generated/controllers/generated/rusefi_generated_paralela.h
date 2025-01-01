f27260c03a4b45417df1f65fdbd3b755c3fcad8f
GitHub git update Action <action@github.com>
2025-01-01 21:14:53 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_paralela.h
+++ generated/controllers/generated/rusefi_generated_paralela.h
@@ -1804,7 +1804,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2024.12.31.paralela.2193972826"
+#define TS_SIGNATURE "rusEFI master.2025.01.01.paralela.2193972826"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
